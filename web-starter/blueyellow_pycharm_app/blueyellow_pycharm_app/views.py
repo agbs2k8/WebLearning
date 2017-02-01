d61a89caff874a3773c15207e4419a3002054c5f
agbs2k8 <aj.wilson08@gmail.com>
2017-02-01 09:53:42 -0600

    Move working copy into GIT & Add build_cache_id

--- web-starter/blueyellow_pycharm_app/blueyellow_pycharm_app/views.py
+++ web-starter/blueyellow_pycharm_app/blueyellow_pycharm_app/views.py
@@ -1,6 +1,11 @@
 from pyramid.view import view_config
+import blueyellow_pycharm_app.utils
 
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'blueyellow_pycharm_app'}
+    return extend_model({'project': 'blueyellow_pycharm_app'})
+
+def extend_model(model_dict):
+    model_dict['build_cache_id'] = blueyellow_pycharm_app.utils.build_cache_id
+    return model_dict
